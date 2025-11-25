6a9f9af6e9b99b5d07e1ce5453eb105d11661d04
Dennis <35158752+dennischang928@users.noreply.github.com>
2025-11-25 16:27:16 -0500

    Editted the Year, Update the status from HS to College

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -6,12 +6,12 @@ const Contact = () => {
             <div className={styles.content}>
                 <div className={styles.contact}>
                     <a href="https://github.com/dennischang928" target="_blank"><img loading="lazy" src={require("../Images/github-brands 1.svg").default} alt="" /></a>
-                    <a href="https://www.linkedin.com/in/dennis-chang-436834223/" target="_blank"><img loading="lazy" src={require("../Images/linkedin-brands.svg").default} alt="" /></a>
+                    <a href="https://www.linkedin.com/in/dennischang928/" target="_blank"><img loading="lazy" src={require("../Images/linkedin-brands.svg").default} alt="" /></a>
                     <a href="https://www.facebook.com/dennis.chang.1428921/" target="_blank"><img loading="lazy" src={require("../Images/facebook-brands 1.svg").default} alt="" /></a>
                     <a href="https://instagram.com/dennis.s.chon/" target="_blank"><img loading="lazy" src={require("../Images/instagram-brands 1.svg").default} alt="" /></a>
                 </div>
                 <div className={styles.copyright}>
-                    ©2022 by Dennis Chang
+                    ©2025 by Dennis, Seng Chon Chang
                 </div>
             </div>
         </div>
